8d7ec32d4dfe5e2b9e59607a4fa7713510a449c8
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2019-01-28 22:25:32 -0500

    test gitignore

--- app/adapters/google_adapter.rb
+++ app/adapters/google_adapter.rb
@@ -1,6 +1,7 @@
 class GoogleAdapter
   def self.get_places(search)
     key=KeyAdapter.get_key()
+    byebug
     response = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=#{key}")
     json_response = JSON.parse(response.body)
     return json_response
