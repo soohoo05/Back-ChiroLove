1818f8c1cbbb0c91f78453e71afc648e60004466
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2019-02-18 18:17:50 -0500

    search results

--- app/adapters/google_adapter.rb
+++ app/adapters/google_adapter.rb
@@ -1,7 +1,21 @@
 class GoogleAdapter
   def self.get_places(search)
     key=ENV["GOOGLE_KEY"]
-    response = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=chiropractors+near+163-56+20th+rd+whitestone+ny+11357&key=#{key}")
+    response = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=chiropractors+near+#{search}&key=#{key}")
+    json_response = JSON.parse(response.body)
+    return json_response
+  end
+
+  def self.get_more(place_id)
+    key=ENV["GOOGLE_KEY"]
+    response = RestClient.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{key}")
+    json_response = JSON.parse(response.body)
+    return json_response
+  end
+
+  def self.get_places_by_coords(coords)
+    key=ENV["GOOGLE_KEY"]
+    response = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?location=#{coords["lat"]},#{coords["long"]}&query=chiropractors&key=#{key}")
     json_response = JSON.parse(response.body)
     return json_response
   end
--- app/controllers/chiropractors_controller.rb
+++ app/controllers/chiropractors_controller.rb
@@ -1,20 +1,29 @@
 class ChiropractorsController < ApplicationController
   def show
-@results=GoogleAdapter.get_places("dummy")
+@results=GoogleAdapter.get_places(params["search"])
 render json: @results
   end
 
   def find
 
   end
+  def coords
+    @results=GoogleAdapter.get_places_by_coords(params)
+    render json: @results
 
+  end
   def create
 
   end
   def map
-    lat=params["lat"]
-    lng=params["lng"]
-    gma="https://maps.google.com/maps?q="+lat+","+lng+"&hl=es\;z=14&amp\;&output=embed"
+    key=ENV["GOOGLE_KEY"]
+     name=params["name"].split(" ").join("+")
+    gma="https://www.google.com/maps/embed/v1/place?key=#{key}&q=#{name}"
     render json: gma
+  end
+
+  def more
+    @results=GoogleAdapter.get_more(params["place_id"])
+    render json: @results
   end
 end
